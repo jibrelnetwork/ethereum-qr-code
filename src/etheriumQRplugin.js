47f300af7210e64c25c4ad3ab94721852e1a87cc
Mikhail Kuznetcov <michail.kuznetsov@gmail.com>
2017-08-07 14:00:11 +0200

    split string generation and QR

--- src/etheriumQRplugin.js
+++ src/etheriumQRplugin.js
@@ -1,4 +1,6 @@
 import DrawIcon from './tokenIcon';
+import SchemaGenerator from './schemaGenerator';
+
 import stringFunctions, {
     isAddress,
     validateSignature
@@ -17,6 +19,7 @@ const DEFAULTS = {
         scale: 5
     }
 }
+
 /**
  * Main plugin logic
  */
@@ -30,8 +33,7 @@ class EtheriumQRplugin {
      * @returns String
      */
     toAdressString(config) {
-        this.parseRequest(config);
-        return this.produceEncodedValue();
+        return this.produceEncodedValue(config);
     }
     /**
      * 
@@ -42,8 +44,7 @@ class EtheriumQRplugin {
      * @returns Promise
      */
     toCanvas(config) {
-        this.parseRequest(config);
-        const generatedValue = this.produceEncodedValue();
+        const generatedValue = this.produceEncodedValue(config);
         const parentEl = document.querySelector(config.selector);
 
         if (!config.selector || parentEl === null) {
@@ -72,8 +73,7 @@ class EtheriumQRplugin {
      * @returns Promise
      */
     toDataUrl(config) {
-        this.parseRequest(config);
-        const generatedValue = this.produceEncodedValue();
+        const generatedValue = this.produceEncodedValue(config);
 
         return new Promise((resolve, reject) => {
             QRCode.toDataURL(generatedValue, this.options, (err, url) => {
@@ -86,60 +86,22 @@ class EtheriumQRplugin {
         });
     }
 
-    getString() {
-        return this.schemaGenerator(this.data);
-    }
     getJSON() {
-        return JSON.stringify(this.data);
+        return JSON.stringify( getString());
     }
-    produceEncodedValue() {
-        return this.toJSON ? this.getJSON() : this.getString();
+    produceEncodedValue(config) {
+        this.assignPluguinValues(config)
+        const encodedString = new SchemaGenerator(config);
+        return this.toJSON ? JSON.stringify(encodedString) : encodedString;
     }
-    parseRequest(request) {
-        this.validateToField(request.to);
-        this.validateAndSetMode(request);
-        this.assignPluguinValues(request);
-    }
+
     assignPluguinValues(request) {
-        this.data = {};
-        this.data.to = request.to;
-        this.data.gas = parseInt(request.gas) || DEFAULTS.gas;
         this.toJSON = !!request.toJSON;
         this.size = request.size || DEFAULTS.size;
         this.imgUrl = request.imgUrl || false;
         this.options = Object.assign(DEFAULTS.qrCodeOptions, request.options);
-        this.schemaGenerator = stringFunctions[this.mode];
     }
-    validateToField(requestTo) {
-        if (!requestTo || !isAddress(requestTo)) {
-            this.errorCallback('The "to" parameter with a valid Etherium adress is required');
-        }
-    }
-    validateAndSetMode(request) {
 
-        if (request.mode === 'function') {
-            if (request.functionSignature && validateSignature(request.functionSignature)) {
-                this.mode = 'function';
-                this.data.functionSignature = request.functionSignature;
-                return;
-            } else {
-                this.errorCallback('For the `function` mode, the `functionSignature` object is not provided or not valid');
-            }
-        }
-
-        if (request.mode === 'erc20') {
-            if (request.from && isAddress(request.from) && request.value) {
-                this.mode = 'erc20';
-                this.data.from = request.from;
-                if (parseFloat(request.value)) this.data.value = parseFloat(request.value);
-                return;
-            } else {
-                this.errorCallback('For the `erc20` mode, the `from` object is not provided or not valid');
-            }
-        }
-
-        this.mode = 'eth';
-    }
     drawTokenIcon() {
         if (this.imgUrl) {
             const iconDraw = new DrawIcon();
---
+++ src/schemaGenerator.js
@@ -0,0 +1,50 @@
+
+export class SchemaGenerator {
+    constructor(request){
+        this.data = {};
+        this.parseRequest(request);
+        return this.schemaGenerator(this.data)
+    }
+    parseRequest(request) {
+        this.validateToField(request.to);
+        this.validateAndSetMode(request);
+        this.assignPluguinValues(request);
+    }
+    assignPluguinValues(request) {
+        this.data = {};
+        this.data.to = request.to;
+        this.data.gas = parseInt(request.gas) || DEFAULTS.gas;
+        this.schemaGenerator = stringFunctions[this.mode];
+    }
+    validateToField(requestTo) {
+        if (!requestTo || !isAddress(requestTo)) {
+            this.errorCallback('The "to" parameter with a valid Etherium adress is required');
+        }
+    }
+
+    validateAndSetMode(request) {
+
+        if (request.mode === 'function') {
+            if (request.functionSignature && validateSignature(request.functionSignature)) {
+                this.mode = 'function';
+                this.data.functionSignature = request.functionSignature;
+                return;
+            } else {
+                this.errorCallback('For the `function` mode, the `functionSignature` object is not provided or not valid');
+            }
+        }
+
+        if (request.mode === 'erc20') {
+            if (request.from && isAddress(request.from) && request.value) {
+                this.mode = 'erc20';
+                this.data.from = request.from;
+                if (parseFloat(request.value)) this.data.value = parseFloat(request.value);
+                return;
+            } else {
+                this.errorCallback('For the `erc20` mode, the `from` object is not provided or not valid');
+            }
+        }
+
+        this.mode = 'eth';
+    }
+}
